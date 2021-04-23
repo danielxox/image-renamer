05a51c5090c7cf6e238d23a04b5f9c73e103770b
Daniel <danielsveryown@gmail.com>
2021-04-23 23:45:40 +0200

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -4,6 +4,6 @@ for i in *.jpg *.png *.gif
 do
     ext="$(echo "$i" | sed 's/.*\.//g')"
     num_str="$(printf "%03d" $n)"
-    mv "$i" "soy${num_str}.${ext}"
+    mv "$i" "image${num_str}.${ext}"
     n="$(expr 1 + $n)"
 done
